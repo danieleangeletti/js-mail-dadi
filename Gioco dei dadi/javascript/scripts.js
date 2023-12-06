8b91e8d40b338e72b35399594293a2062012a91c
danieleangeletti <angelettidaniele@gmail.com>
2023-12-06 17:12:16 +0100

    console.log() rimossi

--- Gioco dei dadi/javascript/scripts.js
+++ Gioco dei dadi/javascript/scripts.js
@@ -1,17 +1,19 @@
+my_cont = document.querySelector(".container");
+
 const n_player = Math.floor(Math.random() * ((6 + 1) - 1) + 1);
 const n_computer = Math.floor(Math.random() * ((6 + 1) - 1) + 1);
 
-console.log("The player rolled " + n_player);
-console.log("The computer rolled " + n_computer);
+my_cont.innerHTML += "<span>" + "The player rolled " + n_player + ", " + "</span>";
+my_cont.innerHTML += "<span>" + "the computer rolled " + n_computer + "</span>";
 
 if (n_player > n_computer) {
-    console.log("The player won");
+    my_cont.innerHTML += "<h1>" + "The player won" + "</h1>";
 }
 
 else if (n_player == n_computer) {
-    console.log("No one won");
+    my_cont.innerHTML += "<h1>" + "No one won" + "</h1>";
 }
 
 else {
-    console.log("The computer won");
+    my_cont.innerHTML += "<h1>" + "The computer won" + "</h1>";
 }
